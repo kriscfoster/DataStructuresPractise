1ff0ca645d660d83b8cc3312f25f41096e952360
kriscfoster <kfoster@salesforce.com>
2020-10-19 08:05:26 +0100

    use hamcrest matcher assertThat

--- src/com/kriscfoster/Array/SecondLargestElementTest.java
+++ src/com/kriscfoster/Array/SecondLargestElementTest.java
@@ -1,24 +1,25 @@
 package com.kriscfoster.Array;
 
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import static com.kriscfoster.Array.SecondLargestElement.find;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 class SecondLargestElementTest {
 
     @Test
     void findShouldReturnSecondLargest() {
-        Assert.assertEquals(4, find(new int[]{ 1, 2, 3, 4, 5 }));
+        assertThat(4, equalTo(find(new int[]{ 1, 2, 3, 4, 5 })));
     }
 
     @Test
     void findWhenDecreasingOrder() {
-        Assert.assertEquals(4, find(new int[]{ 5, 4, 3, 2, 1 }));
+        assertThat(4, equalTo(find(new int[]{ 5, 4, 3, 2, 1 })));
     }
 
     @Test
     void findWhenLargestIsInMiddle() {
-        Assert.assertEquals(4, find(new int[]{ 1, 2, 5, 4, 3 }));
+        assertThat(4, equalTo(find(new int[]{ 1, 2, 5, 4, 3 })));
     }
 }
